2b40f0d2e2521472d412cc6387cdc141a0b2d668
sptkl <caobaiyue@gmail.com>
2020-08-20 16:18:20 +0000

    first commit to record work

--- config.sh
+++ config.sh
@@ -9,3 +9,19 @@ function set_env {
   done
 }
 set_env .env
+
+function max_bg_procs {
+    if [[ $# -eq 0 ]] ; then
+            echo "Usage: max_bg_procs NUM_PROCS.  Will wait until the number of background (&)"
+            echo "           bash processes (as determined by 'jobs -pr') falls below NUM_PROCS"
+            return
+    fi
+    local max_number=$((0 + ${1:-0}))
+    while true; do
+            local current_number=$(jobs -pr | wc -l)
+            if [[ $current_number -lt $max_number ]]; then
+                    break
+            fi
+            sleep 1
+    done
+}
--- social_distancing/runner.sh
+++ social_distancing/runner.sh
@@ -1,20 +1,5 @@
 #!/bin/bash
 source config.sh
-function max_bg_procs {
-    if [[ $# -eq 0 ]] ; then
-            echo "Usage: max_bg_procs NUM_PROCS.  Will wait until the number of background (&)"
-            echo "           bash processes (as determined by 'jobs -pr') falls below NUM_PROCS"
-            return
-    fi
-    local max_number=$((0 + ${1:-0}))
-    while true; do
-            local current_number=$(jobs -pr | wc -l)
-            if [[ $current_number -lt $max_number ]]; then
-                    break
-            fi
-            sleep 1
-    done
-}
 
 BASEPATH=sg/sg-c19-response/social-distancing/v2/
 for INFO in $(mc ls --recursive --json $BASEPATH)
