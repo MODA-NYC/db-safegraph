096577bf3944a2093ac98dd7eb783e6b486b6795
mgraber <molly.graber@me.com>
2020-10-15 14:30:50 +0000

    Add partiion when calculating date

--- recipes/daily_zip_poivisits_by_sector.py
+++ recipes/daily_zip_poivisits_by_sector.py
@@ -38,7 +38,7 @@ OUTPUTS:
 
 query = """
 WITH daily_visits AS(
-SELECT safegraph_place_id, poi_cbg, postal_code, date_add('day', row_number() over(), date_start) AS date_current, CAST(visits AS SMALLINT) as visits
+SELECT safegraph_place_id, poi_cbg, postal_code, date_add('day', row_number() over(partition by safegraph_place_id), date_start) AS date_current, CAST(visits AS SMALLINT) as visits
 FROM (
   SELECT
      safegraph_place_id,
