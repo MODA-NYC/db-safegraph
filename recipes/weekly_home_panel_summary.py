f8a98577903e9d8992d08547495a9601627b6ead
sptkl <caobaiyue@gmail.com>
2020-10-23 14:54:42 +0000

    daily to weekly

--- recipes/weekly_home_panel_summary.py
+++ recipes/weekly_home_panel_summary.py
@@ -8,11 +8,12 @@ SELECT
         LPAD(CAST(EXTRACT(week from CAST(SUBSTR(date_range_start, 1, 10) AS DATE)) AS VARCHAR),2,'0') as year_week,
     SUM(number_devices_residing) as number_devices_residing
 FROM home_panel_summary
-WHERE SUBSTR(census_block_group, 1, 5) IN ('36085','36081','36061','36047','36005')
+WHERE SUBSTR(census_block_group, 1, 5) IN ('36085','36081','36061','36047','36005') 
+    AND dt >= CAST('2019-01-01' as DATE)
 GROUP BY SUBSTR(date_range_start, 1, 10)
 ORDER BY SUBSTR(date_range_start, 1, 10)
 """
-tablename1 = 'daily_nyc_summary'
+tablename1 = 'weekly_nyc_summary'
 aws.execute_query(
     query=query1, 
     database="safegraph", 
@@ -40,12 +41,13 @@ SELECT
     SUM(number_devices_residing) as number_devices_residing
 FROM home_panel_summary
 WHERE SUBSTR(census_block_group, 1, 5) IN ('36085','36081','36061','36047','36005')
+    AND dt >= CAST('2019-01-01' as DATE)
 GROUP BY SUBSTR(date_range_start, 1, 10), SUBSTR(census_block_group, 1, 5)
 ORDER BY SUBSTR(date_range_start, 1, 10), SUBSTR(census_block_group, 1, 5)
 """
-tablename2 = 'daily_nyc_borough_summary'
+tablename2 = 'weekly_nyc_borough_summary'
 aws.execute_query(
-    query=query2, 
+    query=query2,
     database="safegraph", 
     output=f"output/home_panel_summary/{tablename2}/{tablename2}.csv.zip"
 )
