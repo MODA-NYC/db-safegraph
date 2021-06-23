e2ea71a426597bdb99b441affb632575176a4789
Yulia Zamriy <yulia.zamriy@gmail.com>
2021-06-23 11:36:54 -0400

    start sync again

--- _init/home_panel_summary.sql
+++ _init/home_panel_summary.sql
@@ -1,17 +1,16 @@
-CREATE EXTERNAL TABLE IF NOT EXISTS safegraph.home_panel_summary (
+CREATE EXTERNAL TABLE IF NOT EXISTS safegraph.home_panel_summary_new (
     `date_range_start` string,
     `date_range_end` string,
     `state` string,
     `census_block_group` string,
     `number_devices_residing` int
 )
-PARTITIONED BY (dt DATE)
 ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde'
 WITH SERDEPROPERTIES (
   'serialization.format' = ',',
   'field.delim' = ',',
   'quoteChar' = '"'
-) LOCATION 's3://recovery-data-partnership/home_panel_summary/'
+) LOCATION 's3://recovery-data-partnership/home_panel_summary_new/'
 TBLPROPERTIES (
   'has_encrypted_data'='false',
   "skip.header.line.count"="1"
---
+++ _sync/neighborhood_patterns.sh
@@ -0,0 +1,130 @@
+#!/bin/bash
+SG_BASEPATH_BACKFILL=sg/sg-c19-response/weekly-patterns-delivery-2020-12-backfill/patterns_backfill
+SG_BASEPATH=sg/sg-c19-response/weekly-patterns-delivery-2020-12/weekly/patterns
+RDP_BASEPATH=rdp/recovery-data-partnership/weekly_patterns_new
+function max_bg_procs {
+    if [[ $# -eq 0 ]] ; then
+            echo "Usage: max_bg_procs NUM_PROCS.  Will wait until the number of background (&)"
+            echo "           bash processes (as determined by 'jobs -pr') falls below NUM_PROCS"
+            return
+    fi
+    local max_number=$((0 + ${1:-0}))
+    while true; do
+            local current_number=$(jobs -pr | wc -l)
+            if [[ $current_number -lt $max_number ]]; then
+                    break
+            fi
+            sleep 1
+    done
+}
+
+for INFO in $(mc ls --recursive --json $SG_BASEPATH_BACKFILL)
+do 
+    max_bg_procs 5
+    (
+        KEY=$(echo $INFO | jq -r '.key')
+        NEW_KEY=$(python3 -c "print('$KEY'[14:].replace('/', '-'))")
+        YEARMONTH=$(python3 -c "print('$NEW_KEY'[:7])")
+        FILENAME=$(basename $KEY)
+        # DATE=$(echo $FILENAME | cut -c1-10)
+        # PARTITION="dt=$DATE"
+        # SUBPATH=$(echo $KEY | cut -c-13)
+        echo "NEW_KEY:" $NEW_KEY
+        echo "YEAR MONTH:" $YEARMONTH
+        
+        if [ "${FILENAME#*.}" = "csv.gz" ] && [ $YEARMONTH != '2018-01' ]; then
+            
+            # Check existence
+            # STATUS=$(mc stat --json $RDP_BASEPATH/$PARTITION/$NEW_KEY | jq -r '.status')
+            STATUS=$(mc stat --json $RDP_BASEPATH/$NEW_KEY | jq -r '.status')
+            
+            case $STATUS in
+            success)
+                # echo "$KEY is already synced to $PARTITION/$NEW_KEY, skipping ..."
+                echo "$KEY is already synced to $NEW_KEY, skipping ..."
+            ;;
+            error)
+                # Download data and unzip, remove README.txt and the original .zip file
+                # mc cp $SG_BASEPATH/$KEY $RDP_BASEPATH/$PARTITION/$NEW_KEY
+                mc cp $SG_BASEPATH_BACKFILL/$KEY $RDP_BASEPATH/$NEW_KEY
+            ;;
+            esac
+        else echo "ignore $NEW_KEY"
+        fi
+    ) &
+done
+wait
+echo "Syncing Backfill Complete!"
+
+for INFO in $(mc ls --recursive --json $SG_BASEPATH)
+do 
+    max_bg_procs 5
+    (
+        KEY=$(echo $INFO | jq -r '.key')
+        NEW_KEY=$(python3 -c "print('$KEY'[:11].replace('/', '-')+'$KEY'[14:])")
+        FILENAME=$(basename $KEY)
+        echo "NEW_KEY:" $NEW_KEY
+        
+        if [ "${FILENAME#*.}" = "csv.gz" ]; then
+
+            # Check existence
+            STATUS=$(mc stat --json $RDP_BASEPATH/$NEW_KEY | jq -r '.status')
+            
+            case $STATUS in
+            success)
+                echo "$KEY is already synced to $NEW_KEY, skipping ..."
+            ;;
+            error)
+                # Download data and unzip, remove README.txt and the original .zip file
+                mc cp $SG_BASEPATH/$KEY $RDP_BASEPATH/$NEW_KEY
+            ;;
+            esac
+        else echo "ignore $NEW_KEY"
+        fi
+    ) &
+done
+wait
+echo "Syncing Complete!"
+
+# for INFO in $(mc ls --recursive --json $SG_BASEPATH_NEW)
+# do 
+#     max_bg_procs 5
+#     (
+#         KEY=$(echo $INFO | jq -r '.key')
+#         NEW_KEY=$(python3 -c "print('$KEY'.replace('/', '-'))")
+#         DATE=$(echo $NEW_KEY | cut -c1-10)
+#         PARTITION="dt=$DATE"
+#         FILENAME=$(basename $KEY)
+#         SUBPATH=$(echo $KEY | cut -c-13)
+#         if  [ "${FILENAME#*.}" = "csv.gz" ]; then
+
+#             # Check existence
+#             STATUS=$(mc stat --json $RDP_BASEPATH/$PARTITION/$NEW_KEY | jq -r '.status')
+            
+#             case $STATUS in
+#             success)
+#                 echo "$KEY is already synced to $PARTITION/$NEW_KEY, skipping ..."
+#             ;;
+#             error)
+#                 mkdir -p tmp
+#                 mc cp $SG_BASEPATH_NEW/$KEY tmp/$FILENAME
+#                 (
+#                     # Remove placekey, effective after 2020 october
+#                     cd tmp
+#                     gunzip $FILENAME
+#                     CSVNAME=$(python3 -c "print('$FILENAME'.replace('.gz', ''))")
+#                     cut -f1 -d, --complement $CSVNAME > _$CSVNAME
+#                     rm $CSVNAME
+#                     gzip _$CSVNAME
+#                 )
+#                 mc cp tmp/_$FILENAME $RDP_BASEPATH/$PARTITION/$NEW_KEY
+#                 rm tmp/_$FILENAME
+#             ;;
+#             esac
+#         else echo "ignore $FILENAME"
+#         fi
+#     ) &
+# done
+
+# wait
+# echo "Syncing New Complete !"
