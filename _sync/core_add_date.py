82b12b5d847a8a6ccc3b08ed0688c12b9439ad07
Yulia Zamriy <yulia.zamriy@gmail.com>
2021-06-29 10:54:53 -0400

    fix typo

--- _sync/core_add_date.py
+++ _sync/core_add_date.py
@@ -8,8 +8,8 @@ print("cwd:", cwd)
 print("filename: ", file_name)
 
 df = pd.read_csv(file_name)
+df['year_month'] = year_month
 print(df.shape)
 print(df.colnames)
-df['year_month'] = year_month
 
 df.to_csv(file_name)
