7adf83000a71813d35c3cd08b27d339e4a6cc6d0
Yulia Zamriy <yulia.zamriy@gmail.com>
2021-07-26 14:38:19 -0400

    update endpoint for weekly patterns and home panel summary

--- _sync/weekly_patterns.sh
+++ _sync/weekly_patterns.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 SG_BASEPATH_BACKFILL=sg/sg-c19-response/weekly-patterns-delivery-2020-12-backfill/release-2021-07/weekly/patterns_backfill
-SG_BASEPATH=sg/sg-c19-response/weekly-patterns-delivery-2021-07/weekly/
+SG_BASEPATH=sg/sg-c19-response/weekly-patterns-delivery-2020-12/release-2021-07/weekly/patterns
 RDP_BASEPATH=rdp/recovery-data-partnership/weekly_patterns_202107
+
 function max_bg_procs {
     if [[ $# -eq 0 ]] ; then
             echo "Usage: max_bg_procs NUM_PROCS.  Will wait until the number of background (&)"
@@ -54,35 +55,35 @@ done
 wait
 echo "Syncing Backfill Complete!"
 
-# for INFO in $(mc ls --recursive --json $SG_BASEPATH)
-# do 
-#     max_bg_procs 5
-#     (
-#         KEY=$(echo $INFO | jq -r '.key')
-#         NEW_KEY=$(python3 -c "print('$KEY'[:11].replace('/', '-')+'$KEY'[14:])")
-#         FILENAME=$(basename $KEY)
-#         echo "NEW_KEY:" $NEW_KEY
+for INFO in $(mc ls --recursive --json $SG_BASEPATH)
+do 
+    max_bg_procs 5
+    (
+        KEY=$(echo $INFO | jq -r '.key')
+        NEW_KEY=$(python3 -c "print('$KEY'[:11].replace('/', '-')+'$KEY'[14:])")
+        FILENAME=$(basename $KEY)
+        echo "NEW_KEY:" $NEW_KEY
         
-#         if [ "${FILENAME#*.}" = "csv.gz" ]; then
+        if [ "${FILENAME#*.}" = "csv.gz" ]; then
 
-#             # Check existence
-#             STATUS=$(mc stat --json $RDP_BASEPATH/$NEW_KEY | jq -r '.status')
+            # Check existence
+            STATUS=$(mc stat --json $RDP_BASEPATH/$NEW_KEY | jq -r '.status')
             
-#             case $STATUS in
-#             success)
-#                 echo "$KEY is already synced to $NEW_KEY, skipping ..."
-#             ;;
-#             error)
-#                 # Download data and unzip, remove README.txt and the original .zip file
-#                 mc cp $SG_BASEPATH/$KEY $RDP_BASEPATH/$NEW_KEY
-#             ;;
-#             esac
-#         else echo "ignore $NEW_KEY"
-#         fi
-#     ) &
-# done
-# wait
-# echo "Syncing Complete!"
+            case $STATUS in
+            success)
+                echo "$KEY is already synced to $NEW_KEY, skipping ..."
+            ;;
+            error)
+                # Download data and unzip, remove README.txt and the original .zip file
+                mc cp $SG_BASEPATH/$KEY $RDP_BASEPATH/$NEW_KEY
+            ;;
+            esac
+        else echo "ignore $NEW_KEY"
+        fi
+    ) &
+done
+wait
+echo "Syncing Complete!"
 
 # for INFO in $(mc ls --recursive --json $SG_BASEPATH_NEW)
 # do 
