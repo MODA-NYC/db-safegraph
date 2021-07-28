0bb4bf767986e39b8002a013906694d26d63fd64
Yulia Zamriy <yulia.zamriy@gmail.com>
2021-07-28 17:04:51 -0400

    1. add neigh.patt. to daily sync; 2.update schema for home panel summary tables

--- _init/np_home_panel_summary.sql
+++ _init/np_home_panel_summary.sql
@@ -1,6 +1,6 @@
-CREATE EXTERNAL TABLE IF NOT EXISTS safegraph.home_panel_summary_202107 (
-  `date_range_start` string,
-  `date_range_end` string,
+CREATE EXTERNAL TABLE IF NOT EXISTS safegraph.neighborhood_home_panel_summary_202107 (
+  `year` int,
+  `month` int,
   `region` string,
   `iso_country_code` string,
   `census_block_group` string,
@@ -12,7 +12,7 @@ WITH SERDEPROPERTIES (
   'serialization.format' = ',',
   'field.delim' = ',',
   'quoteChar' = '"'
-) LOCATION 's3://recovery-data-partnership/home_panel_summary_202107/'
+) LOCATION 's3://recovery-data-partnership/neighborhood_home_panel_summary_202107/'
 TBLPROPERTIES (
   'has_encrypted_data'='false',
   "skip.header.line.count"="1"
