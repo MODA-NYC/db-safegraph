9baff124117aeb96a051f9d0f2253546f93eac42
Yulia Zamriy <yulia.zamriy@gmail.com>
2021-08-12 10:44:04 -0400

    change NYC to NYS for special pull

--- recipes/wp_nyc_202107.py
+++ recipes/wp_nyc_202107.py
@@ -1,28 +1,51 @@
 from _helper import aws
-from _helper.quarters import PastQs, get_quarter
-# from _helper.poi import poi_latest_date
+from calendar import monthrange
 import sys
+import datetime
 
+
+# query = """
+# SELECT *
+# FROM "safegraph"."weekly_patterns_202107"
+# WHERE SUBSTR(poi_cbg,1,5) IN ('36085','36081','36061','36047','36005')  
+#       AND CAST(SUBSTR(date_range_start, 1, 10) AS DATE) >= CAST('{0}' AS DATE)
+#       AND CAST(SUBSTR(date_range_start, 1, 10) AS DATE) <= CAST('{1}' AS DATE) 
+# """
 
 query = """
 SELECT *
 FROM "safegraph"."weekly_patterns_202107"
-WHERE SUBSTR(poi_cbg,1,5) IN ('36085','36081','36061','36047','36005')  
+WHERE region = 'NY'  
       AND CAST(SUBSTR(date_range_start, 1, 10) AS DATE) >= CAST('{0}' AS DATE)
       AND CAST(SUBSTR(date_range_start, 1, 10) AS DATE) <= CAST('{1}' AS DATE) 
 """
 
-# Load the current quarter
-quarters = get_quarter()
+# get today's date to limit the date range for pulled data
+today = datetime.date.today()
+next_month = '-'.join([str(today.year),str(today.month+1).zfill(2),'01'])
+print(next_month)
 
-# quarters = PastQs
+### HISTORICAL WEEKLY PATTERNS FOR NYS
 
-for year_qrtr, range in quarters.items():
-    start = range[0]
-    end = range[1]
-    print(year_qrtr, start, end) 
-    aws.execute_query(
-        query=query.format(start, end), 
-        database="safegraph", 
-        output=f"output/dev/wp_NYC/weekly_patterns_NYC_{year_qrtr}.csv.zip"
-    )
+# date range
+years = list(range(2018,2023))
+months = list(range(1,13))
+
+for y in years:
+    for m in months:
+        # pull number of days in a given month
+        _, numdays = monthrange(y,m)
+        # create a string yyyy-mm
+        data_month = '_'.join([str(y),str(m).zfill(2)])
+        # create a string yyyy-mm-01 for the beginning of the month
+        start = '-'.join([str(y),str(m).zfill(2),'01'])
+        # create a string yyyy-mm-dd for the end of the month
+        end = '-'.join([str(y),str(m).zfill(2),str(numdays)])
+        # pull the data only through the last day of the current month
+        if start < next_month:
+            print(start,end)
+            aws.execute_query(
+                query=query.format(start, end), 
+                database="safegraph", 
+                output=f"output/dev/wp_NYS/weekly_patterns_NYS_{data_month}.csv.zip"
+            )
