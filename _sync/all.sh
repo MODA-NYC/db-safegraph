19f4f735486962a9e073524f3549ebba0803b525
SteveScott <sscott1@cityops.nyc.gov>
2022-05-26 16:39:04 -0400

    maybe if we add s3://

--- _sync/all.sh
+++ _sync/all.sh
@@ -44,7 +44,7 @@ echo "copying..."
 #copy_sg safegraph-places-outgoing/nyc_gov/weekly /safegraph-post-rdp/patterns
 #copy_sg safegraph-places-outgoing/neighborhood-patterns/release-2021-07-01 /safegraph-post-rdp/neighborhood-patterns/r2021-07/
 #let try to force it.
-#mc cp --recursive safegraph-places-outgoing/nyc_gov/weekly /safegraph-post-rdp/patterns
-aws s3 sync s3://safegraph-places-outgoing/nyc_gov/weekly s3://safegraph-post-rdp/patterns
+mc cp --recursive s3://safegraph-places-outgoing/nyc_gov/weekly s3://safegraph-post-rdp/patterns
+#aws s3 sync s3://safegraph-places-outgoing/nyc_gov/weekly s3://safegraph-post-rdp/patterns
 wait
 echo "raw data sync is complete"
