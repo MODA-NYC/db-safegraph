d13727e4e6ad77def01d40151fd576b1e56564c6
SteveScott <sscott1@cityops.nyc.gov>
2022-05-26 17:24:36 -0400

    Key is not the secret key. Key is directory.

--- _sync/all.py
+++ _sync/all.py
@@ -3,8 +3,8 @@ import boto3
 s3 = boto3.resource('s3')
 KEY=os.getenv('SG_SECRET_ACCESS_KEY')
 copy_source = {
-    'Bucket': 'safegraph-places-outgoing/nyc_gov/weekly',
-    'Key': KEY
+    'Bucket': 'safegraph-places-outgoing',
+    'Key': 'nyc_gov/weekly'
 }
-bucket = s3.Bucket('safegraph-post-rdp/patterns')
-bucket.copy(copy_source, KEY) #other key. In this case the same.
+bucket = s3.Bucket('safegraph-post-rdp')
+bucket.copy(copy_source, 'patterns') #other key. In this case the same.
