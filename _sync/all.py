206a5b043c558655c0c39e5357faf9d97d6d9ee2
SteveScott <sscott1@cityops.nyc.gov>
2022-05-26 17:15:45 -0400

    remove s3://?

--- _sync/all.py
+++ _sync/all.py
@@ -3,8 +3,8 @@ import boto3
 s3 = boto3.resource('s3')
 KEY=os.getenv('SG_SECRET_ACCESS_KEY')
 copy_source = {
-    'Bucket': 's3://safegraph-places-outgoing/nyc_gov/weekly',
+    'Bucket': 'safegraph-places-outgoing/nyc_gov/weekly',
     'Key': KEY
 }
-bucket = s3.Bucket('s3://safegraph-post-rdp/patterns')
-bucket.copy(copy_source, KEY)
+bucket = s3.Bucket('safegraph-post-rdp/patterns')
+bucket.copy(copy_source, KEY) #other key. In this case the same.
