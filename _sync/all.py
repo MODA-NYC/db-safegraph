454d8e3a31177cf55623642061d9e0ed54f265c6
SteveScott <sscott1@cityops.nyc.gov>
2022-05-27 11:25:12 -0400

    add trailing slash

--- _sync/all.py
+++ _sync/all.py
@@ -8,7 +8,7 @@ s3 = boto3.resource('s3')
 KEY=os.getenv('SG_SECRET_ACCESS_KEY')
 copy_source = {
     'Bucket': 'safegraph-places-outgoing',
-    'Key': 'nyc_gov/weekly'
+    'Key': 'nyc_gov/weekly/'
 }
 bucket = s3.Bucket('safegraph-post-rdp')
-bucket.copy('./example.env', 'patterns') #other key. In this case the same.
+bucket.copy(copy_source, 'patterns') #other key. In this case the same.
