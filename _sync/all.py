965e7c631f8b339fc2807798e821ca05cca006e7
SteveScott <sscott1@cityops.nyc.gov>
2022-05-27 12:39:52 -0400

    session.resource(s3)

--- _sync/all.py
+++ _sync/all.py
@@ -4,8 +4,7 @@ session = boto3.Session(
     aws_access_key_id=f'{os.getenv("SG_ACCESS_KEY_ID")}',
     aws_secret_access_key=f'{os.getenv("SG_SECRET_ACCESS_KEY")}'
 )
-s3 = boto3.resource('s3')
-KEY=os.getenv('SG_SECRET_ACCESS_KEY')
+s3 = session.resource('s3')
 copy_source = {
     'Bucket': 'safegraph-places-outgoing',
     'Key': 'nyc_gov/weekly/'
